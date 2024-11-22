3b2a960154e625b86258356d5e98274939c67e3e
Jim Burlingame <jb@samplx.org>
2024-11-22 13:59:01 -0600

    Enable 404 files.

--- lib/b2again-conventions.ts
+++ lib/b2again-conventions.ts
@@ -479,13 +479,13 @@ export default function getStandardConventions(sourceName: ArchiveSourceName = '
         // interestingLocales -- default to all locales -- can be json w/comments or slugs
         // interestingLocales: getCommonProvider('downloads', 'core', sourceName, `my-locales.jsonc`),
         // missingCore -- url's to ignore since they 404
-        // missingCore: getCommonProvider('downloads', 'core', sourceName, 'core-404.jsonc'),
+        missingCore: getCommonProvider('downloads', 'core', sourceName, 'core-404.jsonc'),
         pluginSlugs: {
             defaults: getCommonProvider('downloads', 'plugins', sourceName, `defaults-list.json`),
             effective: getCommonProvider('downloads', 'plugins', sourceName, `effective-list.json`),
             featured: getCommonProvider('downloads', 'plugins', sourceName, `featured-list.json`),
             interesting: undefined, // getCommonProvider('downloads', 'plugins', sourceName, `my-plugins.jsonc`),
-            missing: undefined, // getCommonProvider('downloads', 'plugins', sourceName, `plugins-404.jsonc`),
+            missing: getCommonProvider('downloads', 'plugins', sourceName, `plugins-404.jsonc`),
             new: getCommonProvider('downloads', 'plugins', sourceName, `new-list.json`),
             popular: getCommonProvider('downloads', 'plugins', sourceName, `popular-list.json`),
             rejected: undefined, // getCommonProvider('downloads', 'plugins', sourceName, `rejected-list.json`),
@@ -496,7 +496,7 @@ export default function getStandardConventions(sourceName: ArchiveSourceName = '
             effective: getCommonProvider('downloads', 'themes', sourceName, `effective-list.json`),
             featured: getCommonProvider('downloads', 'themes', sourceName, `featured-list.json`),
             interesting: undefined, // getCommonProvider('downloads', 'themes', sourceName, `my-themes.jsonc`),
-            missing: undefined, // getCommonProvider('downloads', 'themes', sourceName, `themes-404.jsonc`),
+            missing: getCommonProvider('downloads', 'themes', sourceName, `themes-404.jsonc`),
             new: getCommonProvider('downloads', 'themes', sourceName, `new-list.json`),
             popular: getCommonProvider('downloads', 'themes', sourceName, `popular-list.json`),
             rejected: undefined, // getCommonProvider('downloads', 'themes', sourceName, `rejected-list.json`),
